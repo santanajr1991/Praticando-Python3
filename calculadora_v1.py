fc739e17aa5593c8f4b6aec28cc06b5f7c038eba
santanajr1991 <46023852+santanajr1991@users.noreply.github.com>
2018-12-23 20:21:20 -0200

    Add files via upload

---
+++ calculadora_v1.py
@@ -0,0 +1,26 @@
+# Calculadora em Python
+
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos 2 e 3. 
+# A solução será apresentada no próximo capítulo!
+# Assista o vídeo com a execução do programa!
+
+print("\n******************* Python Calculator *******************")
+
+print("Selecione o número da operação desejada:\n")
+
+print("1 - Soma", "\n2 - Subtração", "\n3 - Multiplicação", "\n4 - Divisão")
+
+A = int(input("Digite sua opção (1/2/3/4): ", )
+
+B = float(input("Digite o primeiro número: ", )
+
+C = float(input("Digite o segundo número: ", )
+
+if A == 1:
+   print(B, "+", C, "=", B + C)
+elif A == 2:
+   print(B, "-", C, "=", B - C)
+elif A == 3:
+   print(B, "x", C, "=", B * C)
+elif A == 4:
+   print(B, "/", C, "=", B / C)
